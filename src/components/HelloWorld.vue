4956495fc944969acda3739bf719de4f527beecc
Patrick Li <1543162420@qq.com>
2022-03-13 17:47:20 -0400

    Init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,101 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row justify="center" class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.jpg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">JI Dorm Book</h1>
+      </v-col>
+
+
+      <v-col cols = '12'>
+        <h3>
+          早餐 Breakfast
+        </h3>
+      </v-col>
+      <v-col cols="7" class="mb-5">
+        <v-select
+          v-model="selectBreakfast"
+          :items="items"
+          item-text="state"
+          item-value="abbr"
+          label="Breakfast"
+          persistent-hint
+          return-object
+          single-line
+        ></v-select>
+      </v-col>
+      <v-col cols = '12'>
+        <h3>
+          中餐 Lunch
+        </h3>
+      </v-col>
+      <v-col cols="7" class="mb-5">
+        <v-select
+          v-model="selectLunch"
+          :items="items"
+          item-text="state"
+          item-value="abbr"
+          label="Lunch"
+          persistent-hint
+          return-object
+          single-line
+        ></v-select>
+      </v-col>
+      <v-col cols = '12'>
+        <h3>
+          晚餐 Dinner
+        </h3>
+      </v-col>
+      <v-col cols="7" class="mb-5">
+        <v-select
+          v-model="selectDinner"
+          :items="items"
+          item-text="state"
+          item-value="abbr"
+          label="Dinner"
+          persistent-hint
+          return-object
+          single-line
+        ></v-select>
+      </v-col>
+      <v-col cols="7" class="mb-5">
+        <v-btn elevation="2"
+         color="primary"
+         @click="submit">提交 Submit</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    selectBreakfast: { state: "不要 No need", abbr: "no" },
+    selectLunch: { state: "不要 No need", abbr: "no" },
+    selectDinner: { state: "不要 No need", abbr: "no" },
+    items: [
+      { state: "不要 No need", abbr: "no" },
+      { state: "普通 Normal", abbr: "normal" },
+      { state: "普通2份 Double sized normal", abbr: "normalDouble" },
+      { state: "清真 Muslim cuisine", abbr: "muslim" },
+      { state: "清真2份 Double sized muslim cuisine", abbr: "muslimDouble" },
+    ],
+  }),
+  methods: {
+    submit() {
+      console.log(this.selectBreakfast.abbr)
+      return
+    }
+  }
+};
+</script>
